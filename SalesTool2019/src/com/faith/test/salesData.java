8a44602492fa20558db3ea4ef785be2470936c21
Irshad <Irshad@10.200.200.108>
2019-10-04 10:40:35 +0530

    second commit

--- SalesTool2019/src/com/faith/test/salesData.java
+++ SalesTool2019/src/com/faith/test/salesData.java
@@ -6,6 +6,7 @@ public class salesData {
 	void display() {
 		// TODO Auto-generated method stub
 		System.out.println("Sales Data......tadaaaaa!!!!");
+		System.out.println("Up ,Up and away....");
 	}
 
 }
