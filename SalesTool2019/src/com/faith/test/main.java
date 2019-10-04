82e400c1cb7d362f48be9e9df665fa123e0e7793
Irshad <Irshad@10.200.200.108>
2019-10-04 10:24:08 +0530

    initial commit

---
+++ SalesTool2019/src/com/faith/test/main.java
@@ -0,0 +1,20 @@
+package com.faith.test;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		salesData objSalesData =new salesData();
+		
+		System.out.println("Hello Ust guys!!!!");
+		displayGreetings();
+		objSalesData.display();
+
+	}
+	
+	private static void displayGreetings(){
+		
+		System.out.println("Welcome to sales report app 2019...");
+	}
+
+}
---
+++ SalesTool2019/src/com/faith/test/salesData.java
@@ -0,0 +1,11 @@
+package com.faith.test;
+
+public class salesData {
+	
+
+	void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Sales Data......tadaaaaa!!!!");
+	}
+
+}
